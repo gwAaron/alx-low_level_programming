c395efeb8bb6ad71a75bcd076f398c65b2f41435
Goodman <aarongoodmandonell@gmail.com>
2022-03-10 18:11:20 -0800

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <unistd.h>
-
 /**
  * main - Prints a quote
  *
@@ -10,4 +9,4 @@ int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
-} 
+}
