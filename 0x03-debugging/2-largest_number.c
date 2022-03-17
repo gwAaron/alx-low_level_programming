458694a98bf6accb85ce0c2da01283551c995af3
gwAaron <aarongoodmandonnell@gmail.com>
2022-03-18 00:11:04 +0300

    task 2

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,17 +12,17 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if (a > b && b > c)
+	if (a >= b && a >= c)
 	{
 		largest = a;
 	}
-	else if (b > a && a > c)
+	if (b >= a && b >= c)
 	{
 		largest = b;
 	}
-	else
+	if (c >= a && c >= b)
 	{
-		largest = 972;
+		largest =  c;
 	}
 	return (largest);
 }
