e97ba5048368f3c43e585e93dabb4714f91a0c26
gwAaron <aarongoodmandonnell@gmail.com>
2022-03-23 01:29:25 +0300

    task-7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,36 +1,26 @@
 #include "main.h"
 
 /**
- * puts_half - print reverse characters.
- * @str: validate the character
- * Return: Always 0.
+ * puts_half - a function that prints half of a string
+ * if odd len, n = (length_of_the_string - 1) / 2
+ * @str: input
+ * Return: half of input
  */
 void puts_half(char *str)
 {
-	int i = 0, n;
+	int i, n, len;
 
+	len = 0;
 
-	while (str[i] != '\0')
-	{
-		i++;
-	}
-	if (i % 2 == 1)
-	{
-		n = (i + 1) / 2;
-		while (str[n] != '\0')
-		{
-		printf(str[n]);
-		n++;
-		}
-	}
-	else
-	{
-		n = (i / 2);
-		while (str[n] != '\0')
-		{
-			printf(str[n]);
-			n++;
-		}
-	}
-	printf('\n');
+	for (i = 0; str[i] != '\0'; i++)
+		len++;
+
+	n = (len / 2);
+
+	if ((len % 2) == 1)
+		n = ((len + 1) / 2);
+
+	for (i = n; str[i] != '\0'; i++)
+		_putchar(str[i]);
+	_putchar('\n');
 }
