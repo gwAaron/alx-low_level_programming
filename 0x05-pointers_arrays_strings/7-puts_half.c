fb00f1fd4d4dc1fc532ed6ac929e02d334154b0a
gwAaron <aarongoodmandonnell@gmail.com>
2022-03-22 04:29:04 +0300

    changes

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -19,7 +19,7 @@ void puts_half(char *str)
 		n = (i + 1) / 2;
 		while (str[n] != '\0')
 		{
-		_putchar(str[n]);
+		putchar(str[n]);
 		n++;
 		}
 	}
@@ -28,9 +28,9 @@ void puts_half(char *str)
 		n = (i / 2);
 		while (str[n] != '\0')
 		{
-			_putchar(str[n]);
+			putchar(str[n]);
 			n++;
 		}
 	}
-	_putchar('\n');
+	putchar('\n');
 }
