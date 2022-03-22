f9ced616c74c0b92412866a045019331826dba7a
gwAaron <aarongoodmandonnell@gmail.com>
2022-03-22 03:22:11 +0300

    task-1

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -6,9 +6,14 @@
  *Return: Always 0 (Success)
  */
 
-void swap(int a, int b)
 {
-	a ^= b; 
-	b ^= a;
-	a ^= b;
+    int a;
+    int b;
+
+    a = 98;
+    b = 42;
+    printf("a=%d, b=%d\n", a, b);
+    swap_int(&a, &b);
+    printf("a=%d, b=%d\n", a, b);
+    return (0);
 }
