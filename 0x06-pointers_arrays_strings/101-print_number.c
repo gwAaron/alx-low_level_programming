6f9faf8e2ad7634cc3a65506cc0e1f691036f99e
gwAaron <aarongoodmandonnell@gmail.com>
2022-03-26 02:09:28 +0300

    file 101

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -5,42 +5,14 @@
  * @n: n -  Variable
  * Return: Always 0.
  */
-void print_number(int n)
+
+int main()
 {
-	unsigned int z;
-	int m, b;
+	int a;
 
-	b = 10;
+	_putchar("Enter an integer\n");
+	scanf("%d", &a);
 
-	if (n < 10 && n >= 0)
-	{
-		_putchar (n + '0');
-	}
-	else if (n > -10 && n < 0)
-	{
-		n = n - 2 * n;
-		_putchar('-');
-		_putchar (n + '0');
-	}
-
-	else
-	{
-		if (n < 0)
-		{
-			n = n * -1;
-			_putchar ('-');
-		}
-		z = n;
-	while (z / b > 9)
-	{
-		b = b * 10;
-	}
-	while (b > 0)
-	{
-		m = z / b;
-		z = z % b;
-		_putchar (m + '0');
-		b = b / 10;
-	}
-	}
+	printf("The integer is: %d\n", a);
+	return 0;
 }
