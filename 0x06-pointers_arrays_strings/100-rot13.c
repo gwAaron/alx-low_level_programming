03412cd51dce2b21320ea0795cc21a0bfd4e08f1
gwAaron <aarongoodmandonnell@gmail.com>
2022-03-26 01:46:11 +0300

    rot-13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -7,13 +7,14 @@
  *
  * Return: Always 0.
  */
+
 char *rot13(char *str)
 {
 	int i = 0, k;
 	char s[] =  "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 	char s1[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
-	for (; str[i] != '\0'; i++)
+	while (; str[i] != '\0'; i++)
 	{
 		for (k = 0; k <= 51; k++)
 		{
