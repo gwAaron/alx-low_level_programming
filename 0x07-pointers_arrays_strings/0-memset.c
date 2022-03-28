45480ed1c73a1381b1379aad1f925afb095da151
gwAaron <aarongoodmandonnell@gmail.com>
2022-03-29 01:01:47 +0300

    task-0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -5,21 +5,25 @@
  * @s: buffer
  * @b: write
  * @n: size
+ *
  * Return: Nothing.
  */
+
 void printArray(int arr[], int n)
 {
-	for (int i=0; i<n; i++)
-		printf("%d ", arr[i]);
+   for (int i=0; i<n; i++)
+      _putchar("%d ", arr[i]);
 }
-
+  
 int main()
 {
-	int n = 10;
-	int arr[n];
-	memset(arr, 0, n*sizeof(arr[0]));
-	printf("Array after memset()\n");
-	printArray(arr, n);
-
-	return 0;
+    int n = 10;
+    int arr[n];
+  
+    // Fill whole array with 0.
+    memset(arr, 0, n*sizeof(arr[0]));
+    _putchar("Array after memset()\n");
+    printArray(arr, n);
+  
+    return 0;
 }
