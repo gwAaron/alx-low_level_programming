6f2cc977fd828429dcb673766864635ee2bc9658
gwAaron <aarongoodmandonnell@gmail.com>
2022-03-31 02:12:55 +0300

    task-7

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -38,3 +38,21 @@ int check_palindrome(char *s)
 
 	return (1);
 }
+
+/**
+  * _strlen_recursion - Get the length of a string
+  * @s: the string to get the length
+  *
+  * Return: the string length
+  */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+
+	s++;
+	return (_strlen_recursion(s) + 1);
+}
+
